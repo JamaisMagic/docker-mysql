72e8fd41143578de353eef17a5b8331d8537f6c4
Jamais <wuweikang@cmcm.com>
2018-10-11 14:29:26 +0800

    Update databases infos.

--- src/docker-entrypoint-initdb.d/init_www_namie_cc.sql
+++ src/docker-entrypoint-initdb.d/init_www_namie_cc.sql
@@ -1,8 +1,7 @@
-create database if not exists cc_namie_normal default character set utf8;
+create database if not exists www_picoluna_com default character set utf8;
+create database if not exists www_namie_cc default character set utf8;
 
-use cc_namie_normal;
-
-create table if not exists url (
+create table if not exists www_namie_cc.url (
         `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
         `url` TEXT NOT NULL,
         `ip` varchar(128) CHARACTER SET utf8 NOT NULL,
@@ -12,13 +11,13 @@ create table if not exists url (
         PRIMARY KEY (`id`)
     )ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-create table url_0 like url;
-create table url_1 like url;
-create table url_2 like url;
-create table url_3 like url;
-create table url_4 like url;
-create table url_5 like url;
-create table url_6 like url;
-create table url_7 like url;
-create table url_8 like url;
-create table url_9 like url;
+create table www_namie_cc.url_0 like www_namie_cc.url;
+create table www_namie_cc.url_1 like www_namie_cc.url;
+create table www_namie_cc.url_2 like www_namie_cc.url;
+create table www_namie_cc.url_3 like www_namie_cc.url;
+create table www_namie_cc.url_4 like www_namie_cc.url;
+create table www_namie_cc.url_5 like www_namie_cc.url;
+create table www_namie_cc.url_6 like www_namie_cc.url;
+create table www_namie_cc.url_7 like www_namie_cc.url;
+create table www_namie_cc.url_8 like www_namie_cc.url;
+create table www_namie_cc.url_9 like www_namie_cc.url;
